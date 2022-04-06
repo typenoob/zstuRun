800c6d05f2628b93667c697669c01a89f2cd8f5b
coyote <chenyutao0706@gmail.com>
2022-04-06 21:55:31 +0800

    generate random time in valid scope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import time
 import requests
+import random
 from divide import Region
 from person import Person
 from distance import get_distance_wgs84
@@ -41,6 +42,16 @@ if __name__ == '__main__':
         dic['endtime'] = str(now)
         dic['usetime'] = str(int(dic['endtime'])-int(dic['begintime']))
         return location
+    # input a YYYY-MM-DD return the timestamp
+
+    def getTimeStamp(date):
+        date = date.split('-')
+        return int(time.mktime(time.strptime(date[0]+'-'+date[1]+'-'+date[2], '%Y-%m-%d')))
+
+    # return today in YYYY-MM-DD
+
+    def getToday():
+        return time.strftime('%Y-%m-%d', time.localtime(time.time()))
     while(1):
         with open('./config.json', 'r', encoding='utf8')as fp:
             config = json.load(fp)
@@ -48,7 +59,8 @@ if __name__ == '__main__':
         if day == -1:
             break
         dic = {}
-        dic['begintime'] = str(int(time.time())-day*86400)  # starttime
+        dic['begintime'] = getTimeStamp(
+            getToday())+20*3600+random.randint(0, 3600)-day*24*3600  # starttime
         dic['uid'] = r'd6220f10-d208-43cd-a53e-2aa53685323fb88f31121e714f3da11c9e5c6bb2c87f1622117480$a98bcd3166022036e4ed7d2585d84e2c'
         dic['schoolno'] = '10338'
         dic['distance'] = str(config['target'])
